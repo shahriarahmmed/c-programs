2f19e31f38b3fd6ff81c397cf79b9ae7ba379a6a
Shahriar Ahmmed <shahariarahmmed.sa@gmail.com>
2022-12-22 17:25:06 +0600

    sum of five number program

---
+++ sum-of-five-num.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+ main()
+{
+    int a,b,c,d,e,sum;
+    printf("Give me 5 number:");
+    scanf("%d%d%d%d%d", &a,&b,&c,&d,&e);
+    sum = a+b+c+d+e;
+    printf("sum of five number: %d", &sum);
+    return 0;
+}
